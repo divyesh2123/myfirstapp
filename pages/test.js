1f8f887ca1ee66f70e18a65366522e77de2927da
lenovo <lenovo@LAPTOP-BLNTEBH7>
2022-09-16 11:31:36 +0530

    ths

--- pages/test.js
+++ pages/test.js
@@ -2,10 +2,10 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Test() {
   return (
-    <div className={styles.container}>
-          This is the first application
+    <div >
+        I am the test
     </div>
   )
 }
